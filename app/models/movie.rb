2edb4d7a56d7d6cf77ef471cef11242e169c912a
Mitchell <Hmitchell.hall@gmail.com>
2021-11-29 13:06:58 -0600

    first part passing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,12 @@
 class Movie < ActiveRecord::Base
+    attr_accessor :title
+    @all = []
+    def self.create_with_title(title)
+        @title = title
+        @all << self
+    end
+    def self.first_movie
+
+    end
 
 end
